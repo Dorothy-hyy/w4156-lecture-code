83bb941dde7b23db4ee5ef4fdb91fa4b50e0184a
DorothyHYY <Doro_hyy@163.com>
2018-02-06 00:49:06 -0500

    part 1-3; half of 4

--- tests/testing/theory/test_mood_calculator.py
+++ tests/testing/theory/test_mood_calculator.py
@@ -1,6 +1,6 @@
 import unittest
 from lectures.testing.theory.mood_calculator import MoodCalculator
-
+from lectures.testing.theory.mood_calculator import Mood
 
 class MyTestCase(unittest.TestCase):
     """
@@ -11,16 +11,60 @@ class MyTestCase(unittest.TestCase):
         self.mood_calculator = MoodCalculator()
 
     def test_out_of_range(self):
-        with self.assertRaises(ValueError):
-            self.mood_calculator.calculate_mood(-1, 70)
+        caseserror1 = [
+            (-1, 69),
+            (-1, 70),
+            (-1, 90),
+            (-1, 91),
+            (37, 69),
+            (37, 70),
+            (37, 76),
+            (37, 91)
+        ]
+        caseserror2 = [
+            (0, 69),
+            (0, 91),
+            (27, 69),
+            (27, 91),
+        ]
+        for case in caseserror1:
+            with self.assertRaises(ValueError):
+                self.mood_calculator.calculate_mood(case[0], case[1])
+        for case in caseserror2:
+            with self.assertRaises(ValueError):
+                self.mood_calculator.calculate_mood(case[0], case[1])
 
     def test_mood_calculator(self):
         """
         TODO - Exercise for the student to write and test the mood calculator
         :return:
         """
-        pass
+        caseIrri = [
+            (0, 70),
+        ]
+        caseJoy = [
+            (0, 76),
+        ]
+        caseMoo = [
+            (27, 70),
+        ]
+        caseHul = [
+            (27, 76),
+        ]
 
+
+        for case in caseIrri:
+            mood = self.mood_calculator.calculate_mood(case[0], case[1])
+            self.assertEqual(mood, Mood(3))
+        for case in caseJoy:
+            mood = self.mood_calculator.calculate_mood(case[0], case[1])
+            self.assertEqual(mood, Mood(1))
+        for case in caseMoo:
+            mood = self.mood_calculator.calculate_mood(case[0], case[1])
+            self.assertEqual(mood, Mood(2))
+        for case in caseHul:
+            mood = self.mood_calculator.calculate_mood(case[0], case[1])
+            self.assertEqual(mood, Mood(4))
 
 if __name__ == '__main__':
     unittest.main()
--- tests/testing/theory/test_plane_navigation_computer.py
+++ tests/testing/theory/test_plane_navigation_computer.py
@@ -4,6 +4,8 @@ import tests.helper as helper
 
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.nearest_enemy = NearestEnemyFinder()
 
     @helper.skip_intentionally_failing()
     def test_todo(self):
@@ -11,7 +13,28 @@ class MyTestCase(unittest.TestCase):
         Exercise for the student
         :return:
         """
-        self.assertTrue(False)
+
+        casesempty = [
+            [0,[RadarSignature(0, 0, FriendFoe(2))] ]
+
+        ]
+        caseserror = [
+            [-1, [RadarSignature(0, 0, FriendFoe(2))]],
+            [1, [RadarSignature(0, 0, FriendFoe(1)), RadarSignature(1, 1, FriendFoe(1))]]
+        ]
+        cases = [[1, [RadarSignature(0, 0,FriendFoe(1)),RadarSignature(1,1,FriendFoe(2))]],
+                 [1, [RadarSignature(0, 0, FriendFoe(2)), RadarSignature(1, 1, FriendFoe(2))]],
+                 [5, [RadarSignature(0, 0, FriendFoe(1)), RadarSignature(1, 1, FriendFoe(2)),RadarSignature(2, 2, FriendFoe(1)), RadarSignature(3, 3, FriendFoe(2)),
+                      RadarSignature(4, 4, FriendFoe(2)), RadarSignature(5, 5, FriendFoe(2)),RadarSignature(6, 6, FriendFoe(1)), RadarSignature(7, 7, FriendFoe(2))]],
+                 [1, [RadarSignature(0, 0, FriendFoe(1)), RadarSignature(9999999999999999, 99999999999999999999, FriendFoe(2))]]
+                 ]
+        for case in caseserror:
+            res = self.nearest_enemy.detect_nearest_enemy(case[0], case[1])
+            self.assertEqual(res,"Error")
+        for case in casesempty:
+            res = self.nearest_enemy.detect_nearest_enemy(case[0], case[1])
+            self.assertEqual(res,[])
+
 
 if __name__ == '__main__':
     unittest.main()
